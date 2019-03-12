5ffdf858e1b9b760366d6ca70a78b3e0344642d1
Chris Riederer <christopher.riederer@gmail.com>
2019-03-11 20:34:39 -0700

    Adding figures and doing some formatting

--- process_json.py
+++ process_json.py
@@ -1,7 +1,9 @@
 import json
 
+import matplotlib.style as style
 import numpy as np
 import pandas as pd
+import pylab as pl
 
 
 def make_rows(cngrs_prsn):
@@ -34,7 +36,9 @@ def make_rows(cngrs_prsn):
 
 
 def load_df_from_files():
-  """Create a DataFrame 
+  """Create a DataFrame where each row contains information on a 
+  Congressperson's age on December 31st for each year in which he or she is in 
+  office.
   """
   with open("legislators-historical.json") as f:
     data_old = json.load(f)
@@ -65,7 +69,12 @@ def clean_df(df):
 
 
 def expand_df_dates(df):
-  # Nice answer courtesy of https://stackoverflow.com/questions/43832484/expanding-a-dataframe-based-on-start-and-end-columns-speed
+  """Expand the dataframe so that each row has the age of a Congressperson in a
+  particular year.
+  
+  This code based on: 
+  https://stackoverflow.com/questions/43832484/expanding-a-dataframe-based-on-start-and-end-columns-speed
+  """
   dates = [pd.bdate_range(r[0], r[1], freq="A").to_series() 
       for r in df[['term_start', 'term_end']].values]
 
@@ -83,46 +92,114 @@ def create_df():
   df = clean_df(df)
   df = expand_df_dates(df)
   df["age_at_t"] = ((df["date"] - df["birthday"]) / 365).dt.days  # Yeah, this is weird.
-
   return df
 
 
-df = df[df["date"] <= "2018"]
+# Load that data
+df = create_df()
 
-pl.figure()
-df.groupby("date").age_at_t.mean().plot()
+
+# Limit to when next term ends (as of time of writing, 2019-03-09)
+df = df[df["date"] <= "2020-12-31"]
+
+
+# Set the style
+style.use("seaborn-whitegrid")
+
+
+# Overall average age
+df.groupby("date").age_at_t.mean().plot(figsize=(8, 4))
 pl.title("Average Age of Congress")
+pl.ylabel("Average Age")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_avgage.png")
 
 
-df.groupby(["date", "term_type"]).age_at_t.mean().unstack().plot()
+# Mean and Median
+tmp = df.groupby("date").agg({"age_at_t": ["mean", "median"]}).plot()
+pl.title("Average and Median Age of Congress")
+
+
+# Age by Senate vs. House
+tmp = (df
+  .groupby(["date", "term_type"])
+  .age_at_t
+  .mean()
+  .unstack())
+tmp.columns = ["House", "Senate"]
+tmp.plot(figsize=(8, 4))
 pl.title("Average Age of Congress by House")
 pl.ylabel("Average Age")
-pl.savefig("time_avgage_byhouse.png")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_avgage_byhouse.png")
 
 
-df.groupby(["date", "gender"]).age_at_t.mean().unstack().plot()
+# Age by Gender
+(df
+  .groupby(["date", "gender"])
+  .age_at_t
+  .mean()
+  .unstack()
+  .plot(figsize=(8, 4)))
 pl.title("Average Age of Congress by Gender")
 pl.ylabel("Average Age")
-pl.savefig("time_avgage_bygender.png")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_avgage_bygender.png")
 
 
-df[df.age_at_t > 0].groupby(["date"]).agg({"age_at_t": ["max", "min"]}).plot()
-pl.title("Min/Max Age of Congress")
+# Min and Max Age
+# df[df.age_at_t > 0].groupby(["date"]).agg({"age_at_t": ["max", "min"]}).plot(figsize=(8, 4))
+tmp = (df
+  .groupby(["date"])
+  .agg({"age_at_t": ["max", "min"]})
+  .plot(figsize=(8, 4)))
+tmp.columns = ["Min", "Max"]
+pl.title("Min and Max Age of Congress")
 pl.ylabel("Age")
-pl.savefig("time_minmaxage.png")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_minmaxage.png")
 
 
-# Add in life expectancy
-life = pd.read_csv("life_expectancy.csv")
-life.Year = pd.to_datetime(life.Year.astype(str))
+tmp = (df[df.date >= "1900"]
+  .groupby(["date"])
+  .agg({"age_at_t": ["max", "min"]})
+  .plot(figsize=(8, 4)))
+tmp.columns = ["Min", "Max"]
+pl.title("Min and Max Age of Congress")
+pl.ylabel("Age")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_minmaxage_filtered.png")
 
-le = life[(life.Race == "All Races")
-    & (life.Sex == "Both Sexes")]
 
-le.columns = ["year", "race", "sex", "avg_life_exp", "death_rate"]
+# Age by Party
+# Yeah this doesn't look very good.
+(df
+  .groupby(["date", "party"])
+  .age_at_t
+  .mean()
+  .unstack()
+  .plot())
+pl.title("Average Age of Congress by Party")
+pl.ylabel("Average Age")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_avgage_byparty_all.png")
 
-le.year = pd.to_datetime(le.year)
 
-ax = df.groupby("date").age_at_t.mean().plot()
-le.plot(x="year", y="avg_life_exp", ax=ax)
-
+# Age by Dem v Rep
+(df[df.party.isin(["Democrat", "Republican", "Independent"])]
+  .groupby(["date", "party"])
+  .age_at_t
+  .mean()
+  .unstack()
+  .plot())
+pl.title("Average Age of Congress by (some) Party")
+pl.ylabel("Average Age")
+pl.xlabel("Date")
+pl.tight_layout()
+pl.savefig("fig/time_avgage_byparty_some.png")
